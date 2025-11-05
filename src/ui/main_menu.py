7908bc6c035ad579f9e232581f8b18dade20b182
Lo Fl <lflora@cub.uca.edu>
2025-11-05 01:48:49 -0600

    Refactored weird planet creating from planet data in engine.py + small changes to fix my pylint isssues + random python files I didn't make

--- src/ui/main_menu.py
+++ src/ui/main_menu.py
@@ -47,11 +47,14 @@ class MainMenu:
 
         #draw a few more dummy buttons
         self.draw_button(bx, by+80, bw, bh, 0.2, 0.6, 1.0)
-        self.draw_text("dummy button 1", bx + bw/2, by + 80 + bh/2 + 15, 0.0, 0.0, 0.0, align="center")
+        self.draw_text("dummy button 1", bx + bw/2, by + 80 + bh/2 + 15,
+                       0.0, 0.0, 0.0, align="center")
         self.draw_button(bx, by+160, bw, bh, 0.2, 0.6, 1.0)
-        self.draw_text("dummy button 2", bx + bw/2, by + 160 + bh/2 + 15, 0.0, 0.0, 0.0, align="center")
+        self.draw_text("dummy button 2", bx + bw/2, by + 160 + bh/2 + 15,
+                       0.0, 0.0, 0.0, align="center")
         self.draw_button(bx, by+240, bw, bh, 0.2, 0.6, 1.0)
-        self.draw_text("dummy button 3", bx + bw/2, by + 240 + bh/2 + 15, 0.0, 0.0, 0.0, align="center")
+        self.draw_text("dummy button 3", bx + bw/2, by + 240 + bh/2 + 15,
+                       0.0, 0.0, 0.0, align="center")
 
         # restore previous GL state
         gl.glEnable(gl.GL_DEPTH_TEST)
