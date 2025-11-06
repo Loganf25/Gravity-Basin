81615afbc2358da8a7d1b743a84d3658ee447d25
MG-Kilometer <mglover9@cub.uca.edu>
2025-11-06 11:09:29 -0600

    button refactor, ui changes/improvements

--- src/ui/hud.py
+++ src/ui/hud.py
@@ -6,7 +6,33 @@ class SimulationScreen:
     """Heads-up display for the simulation screen with return to menu button."""
     def __init__(self, engine):
         self.engine = engine
-        self.button_rect = (50, 80, 150, 50)
+
+        #all the buttons on the hud
+        self.button_list = []
+        self.button_list.append((50, 80, 150, 50)) # back button
+
+        #build the time controls buttons
+        #todo
+        self.button_list.append((50, 10, 250, 50)) #temp button (false button)
+        #build the planet controls buttons
+        #todo
+        self.button_list.append((50, 600, 700, 70)) #temp button (false button)
+        #build the object menu buttons
+        #todo
+        self.button_list.append((1000, 10, 260, 700)) #temp button (false button)
+
+        #building the planet adding buttons
+        for i in range(12):
+            if i % 2 == 0:
+                self.button_list.append((1000+20, 33+i*55, 100, 100))
+                self.button_list.append((1000+140, 33+i*55, 100, 100))
+
+
+    """is (x,y) within button(bx,by,bw,bh)"""
+    def within_bounds(self,x,y,bx,by,bw,bh):
+        if bx <= x <= bx + bw and by <= y <= by + bh:
+            return True
+        return False
 
     def update(self, input_handler):
         """Update HUD state based on input."""
@@ -14,26 +40,67 @@ class SimulationScreen:
             self.engine.running = False
             return
 
+        """if mouse if pressed, check button hitbox collisions"""
         if input_handler.mouse_pressed:
+
             x, y = input_handler.mouse_pos
-            bx, by, bw, bh = self.button_rect
-            if bx <= x <= bx + bw and by <= y <= by + bh:
-                self.engine.change_state("menu")
+
+            #for every button
+            for i in range(len(self.button_list)):
+
+                bx, by, bw, bh = self.button_list[i]
+
+                #if mouse within some button
+                if self.within_bounds(x, y, bx, by, bw, bh):
+
+                    #back to menu button
+                    if i == 0:
+                        self.engine.change_state("menu")
+
+                    #all three are debug buttons, they will still render but will actually be ignored
+                    #time controls buttons
+                    if i == 1:
+                        print("clicking time controls box")
+
+                    #planet controls buttons
+                    if i == 2:
+                        print("planet controls box")
+
+                    #object menu buttons
+                    if i == 3:
+                        print("object menu box")
+
+                    #time controls will have a few buttons (pause/play reverse, double speed, half speed)
+                    
+                    #planet controls (will either be entirely informational or allows for modifcation of 
+                    # planet attributes (mass,radius,volume) in a way reminacient of changing magnitudes 
+                    # (so will be doublings and halvings but no input boxes [too complex]))
+
+                    #object menu 1 button for each planet
 
     def render(self):
         """Render the simulation HUD."""
         gl.glClearColor(0.0, 0.0, 0.1, 1)
         gl.glLoadIdentity()
 
-        bx, by, bw, bh = self.button_rect
-        self.draw_button(bx, by, bw, bh, 1.0, 0.8, 0.2)
-        self.draw_text("Return to Menu", bx + bw/2, by + bh/2 + 10, 0.0, 0.0, 0.0, align="center")
+        #draw each button in button list
+        for i in range(len(self.button_list)):
+            bx, by, bw, bh = self.button_list[i]
 
-        #place in ui elements approximate positions
-        self.draw_button_text("[time controls]",50, 10, 250, 50, 1.0, 0.8, 0.2)
-        self.draw_button_text("[planet controls]",50, 600, 700, 70, 1.0, 0.8, 0.2)
-        self.draw_button_text("[object menu]",1000, 10, 260, 700, 1.0, 0.8, 0.2)
+            if i == 0:
+                #place in back to menu button
+                self.draw_button(bx, by, bw, bh, 1,0,0)
+                self.draw_text("Return to Menu", bx + bw/2, by + bh/2 + 10, 0.0, 0.0, 0.0, align="center")
+            if i > 0:
+                #place in rects for each ui element zone, with white trim
+                self.draw_trimmed_button(bx, by, bw, bh, 0, 0, 0, 1, 1, 1)
 
+    def draw_trimmed_button(self, x, y, w, h, r, g, b, tr, tg, tb):
+        """draw a button with some trim to it"""
+        self.draw_button(x-1,y-1,w+2,h+2, tr,tg,tb) #colored trim
+        self.draw_button(x,y,w,h, r,g,b)    #main rect
+        
+    
     def draw_button_text(self, text, x, y, w, h, r, g, b):
         """Draw a button with text."""
         self.draw_button(x,y,w,h, r,g,b)
