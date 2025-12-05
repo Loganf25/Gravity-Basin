0dda60bd881ae07a4e9c2d4af61d340445514812
cassie <cleder@cub.uca.edu>
2025-12-05 14:08:28 -0600

    Update system_test.py

--- tests/system_test.py
+++ tests/system_test.py
@@ -1,7 +1,13 @@
 """System tests for the Universe Simulator"""
 import pytest
-from src import main
+from src.core.engine import Engine
+from src.core.states import States
 
 @pytest.fixture
 def testSystem():
-    main.main()
+    engine = Engine()
+    engine.setup_opengl()
+    engine.initialize_simulation()
+    engine.change_state(States.MENU) # menu should now pop-up
+    engine.change_state(States.SIMULATION) # simulation should begin
+    engine.change_state(States.EXIT) # program should terminate
